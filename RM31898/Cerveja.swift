1b81dbe102b4dd4a8c6646fbbe1fea537a26d07f
Admin <admin@Admins-Mac.local>
2017-12-10 22:53:29 +0300

    first commit

---
+++ RM31898/Cerveja.swift
@@ -0,0 +1,9 @@
+//
+//  Cerveja.swift
+//  RM31898
+//
+//  Created by Admin on 10/12/17.
+//  Copyright © 2017 FiapAluno. All rights reserved.
+//
+
+import Foundation
---
+++ RM31898/CervejaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CervejaTableViewCell.swift
+//  RM31898
+//
+//  Created by Admin on 10/12/17.
+//  Copyright © 2017 FiapAluno. All rights reserved.
+//
+
+import UIKit
+
+class CervejaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
